c5a99df6a7351119655e66d2d49a9d8616599694
SparksFire02 <sparksfiredefault@gmail.com>
2022-03-08 12:43:52 +0800

    yes

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+    $con = mysqli_connect("b82xm3oum4xsphlgwjdb-mysql.services.clever-cloud.com","uejdgg1mcewdxme9","j1uDg3UAryOEBlAgLHWd","b82xm3oum4xsphlgwjdb", "3306");
+    // Check connection
+    if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    require('db.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        Hi
+    </body>
+</html>
